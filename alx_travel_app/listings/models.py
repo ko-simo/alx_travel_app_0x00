45cb86fcaa5829040bf03e2aa2f7f9982dbe3761
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-19 18:04:56 +0100

    Create models.py

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, related_name='bookings', on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=255)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user_name} booking for {self.listing.title}'
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews', on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=255)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review by {self.user_name} for {self.listing.title}'
